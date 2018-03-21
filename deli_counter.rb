e8f62a58839a1eb6d9a7c6adba37acc9aad83003
Spyros Bartokaymenos <tp4417@edu.teicrete.gr>
2018-03-21 18:47:27 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+def line(queue)
+  if queue.empty?
+    puts "The line is currently empty."
+  else
+    str =  "The line is currently: "
+    queue.each_with_index do |name, index|
+      str << "#{index + 1}. #{name}"
+      str << " " 
+    end
+  end
+end
+
+def take_a_number(queue,name)
+  queue << name
+
+  puts "Welcome, #{name}. You are number #{queue.length} in line."
+end
+
+def now_serving(queue)
+  if queue.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{queue.shift}."
+  end
+end
